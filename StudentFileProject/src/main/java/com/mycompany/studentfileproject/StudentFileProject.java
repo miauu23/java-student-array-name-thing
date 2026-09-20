f95cc465c60d945440ac89690aefa0ca1f305ee7
miauu23 <s080797@student.mnu.edu.mv>
2026-09-20 17:42:20 +0500

    Update StudentFileProject.java

--- StudentFileProject/src/main/java/com/mycompany/studentfileproject/StudentFileProject.java
+++ StudentFileProject/src/main/java/com/mycompany/studentfileproject/StudentFileProject.java
@@ -1,38 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.studentfileproject;
+
 import java.io.*;
 import java.util.*;
-/**
- *
- * @author raahil.muumin
- */
+
+
 public class StudentFileProject {
-
     public static void main(String[] args) throws IOException {
-
         Scanner input = new Scanner(System.in);
-
-        ArrayList<Student> students = new ArrayList<>();
-
-        System.out.print("Enter student name: ");
-        String name = input.nextLine();
-
-        System.out.print("Enter student age: ");
-        int age = input.nextInt();
-
-        students.add(new Student(name, age));
-
+        System.out.println("Enter 2 students");
+        Student[] cpt205 = new Student[2];
+        
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Enter student name: ");
+            String name = input.nextLine();
+            System.out.print("Enter student age: ");
+            int age = input.nextInt();
+            input.nextLine();
+            cpt205[i] = new Student(name, age);
+        }
+        
         FileWriter file = new FileWriter("students.txt", true);
-
-        for (Student student : students) {
+        for (Student student : cpt205) {
             file.write(student.name + " " + student.age + "\n");
         }
-
         file.close();
-
-        System.out.println("Student saved.");
+        
+        File fileObj = new File("students.txt");
+        System.out.println("------------------------------------------------------------------------");
+        Scanner fileReader = new Scanner(fileObj);
+        while (fileReader.hasNextLine()) {
+            System.out.println(fileReader.nextLine());
+        }
+        fileReader.close();
     }
 }
